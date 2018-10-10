3245166847298d4cfebd6cda3fd6519c5d28c26c
John Williams <johnwilliams@Johns-MacBook-Pro.local>
2018-10-10 13:08:08 -0600

    Hook up facebook and google authentication to their resepctive buttons

--- src/screens/HikeSettings.js
+++ src/screens/HikeSettings.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
+import { AsyncStorage } from 'react-native';
+import { USER_TOKEN } from '../actions/types';
 
 class HikeSettings extends Component {
+  logOut = () => {
+    AsyncStorage.removeItem(USER_TOKEN);
+    this.props.navigation.navigate('auth');
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>HikeSettings</Text>
+        <Button
+          title="Log out"
+          onPress={this.logOut}
+        />
       </View>
     );
   };
